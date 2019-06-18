e6f9867b48b92b443da8881908f65d1bec727c6f
Steffen Berhorst <Steffen-Berhorst@web.de>
2019-06-18 16:20:26 +0200

    added second motor utility

--- Code/gpio.h
+++ Code/gpio.h
@@ -6,14 +6,20 @@
  */
 
 #ifndef GPIO_H
-#define GPIO_H
+#define	GPIO_H
 #include "xc.h"
 
-#define LED LATBbits.LATB8 // Use Pin 17
+#define LED  LATBbits.LATB8 // Use Pin 17
 
-//******* Define two bits as output bits for the H-Bridge
+//******* Define two bits as output bits for the first H-Bridge
 #define HBRIDGE1 LATBbits.LATB6 // Use Pin 15
 #define HBRIDGE2 LATBbits.LATB7 // Use Pin 16
 
+//******* Define two bits as output bits for the second H-Bridge
+#define HBRIDGE3 LATBbits.LATB4 // Use Pin 11
+#define HBRIDGE4 LATAbits.LATA4 // Use Pin 12
+
+
 void initIO();
-#endif /* GPIO_H */
+#endif	/* GPIO_H */
+
--- Code/pwm.c
+++ Code/pwm.c
@@ -5,25 +5,27 @@
  * Created on 11. Juni 2019, 18:31
  */
 
+
 #include "xc.h"
 #include "gpio.h"
 #include "dma.h"
 #include <stdio.h>
 
+
 void init_PWM()
 {
 
-    /*~~~~~~~~~~~~~~~~~~~~~~~ PWM1 Configuration ~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-
-    P1TCONbits.PTEN = 1; //Disable the PWM Module for configuration
-
-    P1TCONbits.PTMOD = 0; // Define free running mode for PWM 1
-
+    /*~~~~~~~~~~~~~~~~~~~~~~~ PWM1 Configuration ~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/  
+    
+    P1TCONbits.PTEN = 0;    //Disable the PWM Module for configuration
+    
+    P1TCONbits.PTMOD = 0;   // Define free running mode for PWM 1
+    
     PWM1CON1bits.PMOD1 = 1; // Define independent mode for PWM output pair 1
-
+    
     P1TPERbits.PTPER = 100; // PWM will count "up to 100" before resetting
 
-    /* THIS IS THE KEY VARIABLE TO CHANGE SPEED WITH
+	/* THIS IS THE KEY VARIABLE TO CHANGE SPEED WITH
 	* The duty cycle basically calculates as:
 	* P1DC1/PxTPERbits.PTPER , where x denotes the number of the PWM module
 	* Raising PxDCy increases speed and vice versa.
@@ -32,37 +34,55 @@ void init_PWM()
 	* Raising PTPER allows smaller increments (e.g. 100 allows increments of 1/100)
 	* but makes the puls width bigger, decreasing smoothness of V curve
 	*/
-    P1DC1 = 50; // PWM output will be active till count goes to 50
-    // P1DC2 = 50; // Will be active till count goes to 50
-
+	P1DC1 = 50; // PWM output will be active till count goes to 50
+    
+    P1DC3 = 60; // PWM output will be active till count goes to 60
+    
     PWM1CON1bits.PEN1H = 0; // PWM1H is controlled by GPIO module
-
+    
     PWM1CON1bits.PEN1L = 1; // PWM1L is controlled by PWM module
-    // PIN 26 is used as PWM output PIN
-
-    P1TCONbits.PTEN = 1; // Enable the PWM Module
-
+    // PIN 26 is used as PWM output PIN for motor 1
+    
+    PWM1CON1bits.PEN3H = 0; // PWM3H is controlled by GPIO module
+    
+    PWM1CON1bits.PEN3L = 1; // PWM3L is controlled by PWM module
+    // PIN 22 is used as PWM output PIN
+    
+    
+    
+    P1TCONbits.PTEN = 1;    // Enable the PWM Module
+    
     HBRIDGE1 = 1; // An output for H-Bridge logic input
     HBRIDGE2 = 0; // An output for H-Bridge logic input
-
+    
+    HBRIDGE3 = 1; // An output for H-Bridge logic input
+    HBRIDGE4 = 0; // An output for H-Bridge logic input
+    
     // No deadtime inserted yet, as it seems irrelevant right now
 }
 
+
 void test_PWM()
 {
-    if (adcData[0] < 0)
-    {
+    if(adcData[0]<0){
         HBRIDGE1 = 1; // An output for H-Bridge logic input
         HBRIDGE2 = 0; // An output for H-Bridge logic input
+        
+        HBRIDGE3 = 0; // An output for H-Bridge logic input
+        HBRIDGE4 = 1; // An output for H-Bridge logic input
     }
-    else if (adcData[0] < 1000)
-    {
+    else if(adcData[0]<1000){
         HBRIDGE1 = 0; // An output for H-Bridge logic input
         HBRIDGE2 = 1; // An output for H-Bridge logic input
+        
+        HBRIDGE3 = 1; // An output for H-Bridge logic input
+        HBRIDGE4 = 0; // An output for H-Bridge logic input
     }
-    else
-    {
+    else{
         HBRIDGE1 = 1; // An output for H-Bridge logic input
         HBRIDGE2 = 0; // An output for H-Bridge logic input
+        
+        HBRIDGE3 = 0; // An output for H-Bridge logic input
+        HBRIDGE4 = 1; // An output for H-Bridge logic input
     }
 }
